2605132278d5d4bb13c229f7e27524f1bab03f57
Akash Gokral <akashgokral1@gmail.com>
2023-01-11 18:15:26 +0530

    donwe

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,59 +1,113 @@
-
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
 
+  const [email, setemail] = useState("")
+  const [pass, setpass] = useState("")
+  const [emailError, setEmailError] = useState(false)
+  const [passError, setPassError] = useState(false)
+
+  const validateForm = (e) => {
+    e.preventDefault()
+
+    const { email, password } = e.target
+
+    if (email.value === '') {
+      setEmailError(true)
+    }
+    else {
+      setEmailError(false)
+    }
+    if (password.value === '') {
+      setPassError(true)
+    }
+    else {
+      setPassError(false)
+    }
+
+    if (email.value !== '' && password.value !== '') {
+      toast('Login Success', {
+        position: "bottom-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+      setemail('')
+      setpass('')
+    }
+
+  }
+
+
   return (
     <>
-    {/* <div className="container">
-      <div className="row">
-
-      </div>
-    </div> */}
       <div className="login_page">
         <div className="bg_img">
           <img src="Rectangle 1.png" alt="bg_img" />
-          </div>
-          <div>
-            <div className="form">
-              <div className="form_logo">
-                <img src="form_logo.png" alt="" />
-              </div>
-              <h5>Login</h5>
-              <div className="form_bg mb-3">
-                <img src="form_bg.png" alt="form_bg_img" />
-              </div>
-              <div className="form_input mt-3 mb-5">
-                <input type="email" required placeholder="Email" />
-              </div>
-              <div className="form_input mb-3">
-                <input type="password" required placeholder="Password" />
-              </div>
-              <div className="sign_in_btn">
-                <button>Sign In</button>
-              </div>
-              <div className="forgot_pass mt-2">
-                <p>Forgot password</p>
-                <p className="new_user">New User? Sign Up</p>
-              </div>
-              <div>or</div>
-              <div className="buttons mt-3">
-                <button className="mb-3">
-                  <div className="d-flex ms-2 mt-1">
-                    <img src="google.png" alt="google" className="my-1"/>
-                    <p className="d-flex mt-2 w-100 h-100 justify-content-center align-items-center">CONTINUE WITH GOOGLE</p>
-                  </div>
-                  </button>
-                <button>
-                <div className="d-flex ms-2 mt-1">
-                    <img src="fb.png" alt="fb" className="my-1"/>
-                    <p className="d-flex mt-2 w-100 h-100 justify-content-center align-items-center">CONTINUE WITH FACEBOOK</p>
-                  </div>
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
-     
+        <div>
+          <form className="form" onSubmit={validateForm}>
+            <div className="form_logo">
+              <img src="form_logo.png" alt="" />
+            </div>
+            <h5>Login</h5>
+            <div className="form_bg mb-3">
+              <img src="form_bg.png" alt="form_bg_img" />
+            </div>
+            <div className="form_input mb-3 ">
+              <input type="email" placeholder="Email" onChange={(e) => setemail(e.target.value)} name='email' value={email} />
+              {emailError ? <p style={{ color: "red" }}><span className="me-2"><img src="validate.svg"></img></span>Email field is required</p> : null}
+            </div>
+            <div className="form_input mb-3">
+              <input type="password" placeholder="Password" onChange={(e) => setpass(e.target.value)} name='password' value={pass} />
+              {passError ? <p style={{ color: "red" }}><span className="me-2"><img src="validate.svg"></img></span>Password should not be empty</p> : null}
+            </div>
+            <div className="sign_in_btn">
+              <button type="submit">Sign In</button>
+            </div>
+            <div className="forgot_pass mt-2">
+              <p>Forgot password</p>
+              <p className="new_user">New User? Sign Up</p>
+            </div>
+            <div>or</div>
+            <div className="buttons mt-3">
+              <button className="mb-3">
+                <div className="d-flex ms-2 mt-1">
+                  <img src="google.png" alt="google" className="my-1" />
+                  <p className="d-flex mt-2 w-100 h-100 justify-content-center align-items-center">CONTINUE WITH GOOGLE</p>
+                </div>
+              </button>
+              <button>
+                <div className="d-flex ms-2 mt-1">
+                  <img src="fb.png" alt="fb" className="my-1" />
+                  <p className="d-flex mt-2 w-100 h-100 justify-content-center align-items-center">CONTINUE WITH FACEBOOK</p>
+                </div>
+              </button>
+            </div>
+          </form>
+          <ToastContainer
+            position="bottom-left"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+      
+          <ToastContainer />
+        </div>
+      </div>
+
     </>
 
   );
